340bd51279ef44b902c2f0ef6c59476308907e0b
Ruben9911 <ruben.fortes-marques@iesdoctorbalmis.com>
2022-01-17 14:46:47 +0100

    Agregar archivos de proyecto.

---
+++ Personas/contentcontrols/ListaPersonasControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Personas.vms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Personas.contentcontrols
+{
+    /// <summary>
+    /// Lógica de interacción para ListaPersonasControl.xaml
+    /// </summary>
+    public partial class ListaPersonasControl : UserControl
+    {
+        private MainWindowVM vm = new MainWindowVM();
+        public ListaPersonasControl()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
---
+++ Personas/servicios/ServicioNavegacion.cs
@@ -0,0 +1,27 @@
+﻿using Personas.contentcontrols;
+using Personas.vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Personas.servicios
+{
+    class ServicioNavegacion
+    {
+        public ListaPersonasControl listaPersonas { get; set; }
+        public ServicioNavegacion() { }
+
+        // Devuelve un nuevo UserControl de NuevaPersona
+        internal UserControl AbrirNuevaPersona() => new NuevaPersonaControl();
+
+        // Devuelve un nuevo UserControl de ListaPersonas
+        internal UserControl AbrirListaPersonas() => listaPersonas;
+
+        // Abre un diálogo y devuelve el resultado
+        public bool? AbrirAñadirDialog() => new AñadirDialog().ShowDialog();
+        
+    }
+}
